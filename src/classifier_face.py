296e35ccde3b316d1a1d1a475e8a7bd6ef34bbd9
nocater <nocater@163.com>
2018-06-22 10:08:51 +0800

    原始图片按类别文件夹分类

---
+++ src/classifier_face.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import os
+import shutil
+
+ORIGIN_DIR = r'D:\Documents\smartcity\dataset\face_a\train'
+TARGET_DIR = r'D:\Documents\smartcity\dataset\face\train'
+CSV_FILE = r'D:\Documents\smartcity\dataset\face_a\train.csv'
+
+
+read_csv = pd.read_csv(CSV_FILE, header=None)
+
+if not os.path.exists(TARGET_DIR):
+    os.mkdir(TARGET_DIR)
+
+for i in np.arange(len(read_csv)):
+    figure_path_new = os.path.join(TARGET_DIR, read_csv[1][i].astype(np.str))
+    if not os.path.exists(figure_path_new):
+        os.mkdir(figure_path_new)
+    figure_path_original = os.path.join(ORIGIN_DIR, read_csv[0][i])
+    shutil.copy(figure_path_original, figure_path_new)
+
