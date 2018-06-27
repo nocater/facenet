33076bb2fac255c436cf829d018d7b5443e14c56
nocater <nocater@163.com>
2018-06-27 19:25:09 +0800

    使用默认方法进行预测

---
+++ chen/predict_chen.py
@@ -0,0 +1,169 @@
+"""根据模型和标签文件pkl 预测图片类别
+"""
+# MIT License
+#
+# Copyright (c) 2016 David Sandberg
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+import argparse
+import facenet
+import os
+import sys
+import math
+import pickle
+from sklearn.svm import SVC
+import pandas as pd
+
+
+def get_dataset(path, has_class_directories=True):
+    dataset = []
+    path_exp = os.path.expanduser(path)  # 把path中包含的"~"和"~user"转换成用户目录
+
+    facedir = os.path.join(path_exp, "nil")
+    image_paths = facenet.get_image_paths(facedir)
+    dataset.append(facenet.ImageClass("nil", image_paths))
+
+    return dataset
+
+def main(args):
+    with tf.Graph().as_default():
+
+        with tf.Session() as sess:
+
+            np.random.seed(seed=args.seed)
+
+            # 加载数据集 使用无类别方式读取
+            # dataset = facenet.get_dataset(args.data_dir)
+            dataset = get_dataset(args.data_dir)
+
+            # Check that there are at least one training image per class
+            for cls in dataset:
+                assert (len(cls.image_paths) > 0, 'There must be at least one image for each class in the dataset')
+
+            paths, labels = facenet.get_image_paths_and_labels(dataset)
+
+            print('Number of images: %d' % len(paths))
+
+            # Load the model
+            print('Loading feature extraction model')
+            facenet.load_model(args.model)
+
+            # Get input and output tensors
+            images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+            embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+            phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+            embedding_size = embeddings.get_shape()[1]
+
+            # 计算 img2vec
+            # Run forward pass to calculate embeddings
+            print('Calculating features for images')
+            nrof_images = len(paths)
+            nrof_batches_per_epoch = int(math.ceil(1.0 * nrof_images / args.batch_size))
+            emb_array = np.zeros((nrof_images, embedding_size))
+            for i in range(nrof_batches_per_epoch):
+                start_index = i * args.batch_size
+                end_index = min((i + 1) * args.batch_size, nrof_images)
+                paths_batch = paths[start_index:end_index]
+                images = facenet.load_data(paths_batch, False, False, args.image_size)
+                feed_dict = {images_placeholder: images, phase_train_placeholder: False}
+                emb_array[start_index:end_index, :] = sess.run(embeddings, feed_dict=feed_dict)
+
+            classifier_filename_exp = os.path.expanduser(args.classifier_filename)
+
+            # svm比较
+            # Predict images
+            print('Classifier predict')
+            with open(classifier_filename_exp, 'rb') as infile:
+                (model, class_names) = pickle.load(infile)
+
+            print('Loaded classifier model from file "%s"' % classifier_filename_exp)
+
+            predictions = model.predict_proba(emb_array)
+            best_class_indices = np.argmax(predictions, axis=1)
+            best_class_probabilities = predictions[np.arange(len(best_class_indices)), best_class_indices]
+
+            image_names = []
+            image_clas = []
+            for i in range(len(best_class_indices)):
+                # 输出图片名称 类别
+                image_names.append(os.path.basename(dataset[i].image_paths))
+                image_clas.append(class_names[best_class_indices[i]])
+                print('%4d %s %s: %.3f' % (i, os.path.basename(dataset[i].image_paths), class_names[best_class_indices[i]], best_class_probabilities[i]))
+
+            df = pd.DataFrame({'file':image_names,'clas':image_clas})
+            df.to_csv(args.save, index=False)
+
+
+
+def split_dataset(dataset, min_nrof_images_per_class, nrof_train_images_per_class):
+    train_set = []
+    test_set = []
+    for cls in dataset:
+        paths = cls.image_paths
+        # Remove classes with less than min_nrof_images_per_class
+        if len(paths) >= min_nrof_images_per_class:
+            np.random.shuffle(paths)
+            train_set.append(facenet.ImageClass(cls.name, paths[:nrof_train_images_per_class]))
+            test_set.append(facenet.ImageClass(cls.name, paths[nrof_train_images_per_class:]))
+    return train_set, test_set
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('data_dir', type=str,
+                        help='Path to the data directory containing aligned LFW face patches.')
+    parser.add_argument('model', type=str,
+                        help='Could be either a directory containing the meta_file and ckpt_file or a model protobuf (.pb) file')
+    parser.add_argument('classifier_filename',
+                        help='Classifier model file name as a pickle (.pkl) file. ' +
+                             'For training this is the output and for classification this is an input.')
+    parser.add_argument('--save',
+                        help='Predict resutl to csv', default='~/result.csv')
+
+    parser.add_argument('--use_split_dataset',
+                        help='Indicates that the dataset specified by data_dir should be split into a training and test set. ' +
+                             'Otherwise a separate test set can be specified using the test_data_dir option.',
+                        action='store_true')
+    parser.add_argument('--test_data_dir', type=str,
+                        help='Path to the test data directory containing aligned images used for testing.')
+    parser.add_argument('--batch_size', type=int,
+                        help='Number of images to process in a batch.', default=90)
+    parser.add_argument('--image_size', type=int,
+                        help='Image size (height, width) in pixels.', default=250)
+    parser.add_argument('--seed', type=int,
+                        help='Random seed.', default=666)
+    parser.add_argument('--min_nrof_images_per_class', type=int,
+                        help='Only include classes with at least this number of images in the dataset', default=20)
+    parser.add_argument('--nrof_train_images_per_class', type=int,
+                        help='Use this number of images from each class for training and the rest for testing',
+                        default=10)
+
+    return parser.parse_args(argv)
+
+
+if __name__ == '__main__':
+    main(parse_arguments(sys.argv[1:]))
